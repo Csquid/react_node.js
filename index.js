dd856409084641d4a8b73ca597e66f64418c5c43
rmaqlctnekf <rmaqlctnekf@naver.com>
2020-09-27 17:09:55 +0900

    add authentication router, module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const config = require('./config/key')
+const express = require('express');
+const app = express();
+const port = 3000;
+const config = require('./config/key');
+
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
+
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -12,38 +15,42 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 app.use(cookieParser());
 
-const mongoose_account = require('./private/mongoose_account.json');
 const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+
+// connect mongo db
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+        .then(() => console.log('MongoDB Connected...'))
+        .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     
-    //회원 가입 할때 필요한 정보들을 client에서 가져오면
-    // 그것들을 데이터 베이스에 넣어준다.
+    /*
+        회원 가입 할때 필요한 정보들을 client에서 가져오면
+        그것들을 데이터 베이스에 넣어준다.
+    */
     
+    // console.log("const user = new User(req.body);");
+    //이 과정에서 User안에 있는 userSchema 안에 유저의 데이터가 다 들어가게 됨.
     const user = new User(req.body);
+    // console.log("user.name: " + user.name);
 
+    // user.save가 실행되기 전에 plaintext password(평문 텍스트 )
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, error: err});
         //성공했다는 표시
         return res.status(200).json({
-            success: true
+            success: true,
+            result: userInfo
         });
     });
     
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -56,7 +63,7 @@ app.post('/login', (req, res) => {
         //요청한 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
     
         user.comparePassword(req.body.password, (err, isMatch) => {
-            
+
             if(!isMatch)
                 return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다. "});
     
@@ -75,6 +82,24 @@ app.post('/login', (req, res) => {
     });
 
 });
+
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role 0이 아니면 관리자
+
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
